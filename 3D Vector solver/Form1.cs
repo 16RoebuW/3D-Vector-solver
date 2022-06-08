977f7806881d6cdff7756f7d4e3357b60dd83607
16RoebuW <16RoebuW@fulford.york.sch.uk>
2022-06-08 17:59:20 +0100

    Add project files.

---
+++ 3D Vector solver/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3D_Vector_solver
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        double FindUnknown(double coefficient, double value)
+        {
+            return value / coefficient;
+        }
+    }
+
+}
