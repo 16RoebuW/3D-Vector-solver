d056f3cc6d9a9579845b8234d9f484099db24255
William Roebuck <16RoebuW@fulford.york.sch.uk>
2022-06-10 11:46:49 +0100

    Added point/line intersection

--- 3D Vector solver/Form1.Designer.cs
+++ 3D Vector solver/Form1.Designer.cs
@@ -28,20 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cbxOptions = new System.Windows.Forms.ComboBox();
+            this.tbxLn1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxPoint = new System.Windows.Forms.TextBox();
+            this.btnSolve = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // cbxOptions
+            // 
+            this.cbxOptions.FormattingEnabled = true;
+            this.cbxOptions.Items.AddRange(new object[] {
+            "Intersection between a point and a line",
+            "Intersection between two lines",
+            "Are two lines parallel?",
+            "Are two lines skew?"});
+            this.cbxOptions.Location = new System.Drawing.Point(26, 95);
+            this.cbxOptions.Name = "cbxOptions";
+            this.cbxOptions.Size = new System.Drawing.Size(368, 21);
+            this.cbxOptions.TabIndex = 0;
+            // 
+            // tbxLn1
+            // 
+            this.tbxLn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.tbxLn1.Location = new System.Drawing.Point(461, 91);
+            this.tbxLn1.Name = "tbxLn1";
+            this.tbxLn1.Size = new System.Drawing.Size(196, 26);
+            this.tbxLn1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(458, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter a line equation as 6 comma separated values";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(458, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(278, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter another line equation as 6 comma separated values";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox1.Location = new System.Drawing.Point(461, 136);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(458, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(211, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter a point as 3 comma separated values";
+            // 
+            // tbxPoint
+            // 
+            this.tbxPoint.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.tbxPoint.Location = new System.Drawing.Point(461, 181);
+            this.tbxPoint.Name = "tbxPoint";
+            this.tbxPoint.Size = new System.Drawing.Size(196, 26);
+            this.tbxPoint.TabIndex = 5;
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.Location = new System.Drawing.Point(182, 298);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(136, 84);
+            this.btnSolve.TabIndex = 7;
+            this.btnSolve.Text = "Solve!";
+            this.btnSolve.UseVisualStyleBackColor = true;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSolve);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxPoint);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxLn1);
+            this.Controls.Add(this.cbxOptions);
             this.Name = "MainForm";
             this.Text = "Vector Solver";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbxOptions;
+        private System.Windows.Forms.TextBox tbxLn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxPoint;
+        private System.Windows.Forms.Button btnSolve;
     }
 }
 
--- 3D Vector solver/Form1.cs
+++ 3D Vector solver/Form1.cs
@@ -18,9 +18,121 @@ namespace _3D_Vector_solver
 
         }
 
+        private void btnSolve_Click(object sender, EventArgs e)
+        {
+            switch (cbxOptions.Text)
+            {
+                case "Intersection between a point and a line":
+                    {
+                        (bool valid, double pos) returnVal = PointIntersectsLine(ParseLine(tbxLn1.Text), ParsePoint(tbxPoint.Text));
+                        if (returnVal.valid)
+                        {
+                            MessageBox.Show("The point intersects the line when λ = " + returnVal.pos);
+                        }
+                        else
+                        {
+                            MessageBox.Show("The point does not lie on the line");
+                        }
+                        break;
+                    }
+                case "Intersection between two lines":
+                    {
+                        break;
+                    }
+            }
+        }
+
         double FindUnknown(double coefficient, double value)
         {
             return value / coefficient;
+        }
+
+        double[] ParseLine(string line)
+        {
+            string[] splitLine = line.Split(',');
+            double[] output = new double[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                output[i] = Double.Parse(splitLine[i]);
+            }
+
+            return output;
+        }
+
+        double[] ParsePoint(string point)
+        {
+            string[] splitPoint = point.Split(',');
+            double[] output = new double[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                output[i] = Double.Parse(splitPoint[i]);
+            }
+
+            return output;
+        }
+
+        (bool valid, double pos) PointIntersectsLine(double[] line, double[] point)
+        {
+            double coefficient1 = line[3];
+            double value1 = point[0] - line[0];
+            // Find λ for x
+
+            double coefficient2 = line[4];
+            double value2 = point[1] - line[1];
+            // Check λ for y
+
+            if (FindUnknown(coefficient1, value1) == FindUnknown(coefficient2, value2))
+            {
+                return (true, FindUnknown(coefficient1, value1));
+            }
+            else
+            {
+                return (false, 0);
+            }
+        }
+
+        (bool valid, double[] pos) LineIntersection(double[] l1, double[] l2)
+        {
+            double[] eqn1 = new double[3];
+        }
+
+        (bool valid, double x, double y) Find2Unknowns(double[] eqn1, double[] eqn2)
+        {
+            // Construct a 2x2 matrix and invert it
+            double[] inverse = new double[4];
+            inverse[0] = eqn1[0];
+            inverse[1] = eqn1[1];
+            inverse[2] = eqn2[0];
+            inverse[3] = eqn2[1];
+
+            // Determinant
+            double det = inverse[0] * inverse[3] - inverse[2] * inverse[3];
+
+            if (det == 0)
+            {
+                return (false, 0, 0);
+            }
+
+            // Transpose
+            double temp = inverse[0];
+            inverse[0] = inverse[3];
+            inverse[3] = temp;
+
+            // Cofactors
+            inverse[1] *= -1;
+            inverse[2] *= -1;
+
+            for (int i = 0; i < 4; i++)
+            {
+                inverse[i] /= det;
+            }
+
+            double x = inverse[0] * eqn1[2] + inverse[1] * eqn2[2];
+            double y = inverse[2] * eqn1[2] + inverse[3] * eqn2[2];
+
+            return (true, x, y);
         }
     }
 
